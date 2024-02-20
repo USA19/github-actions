ad252b5d11b415d54e973283227617652e9583c0
Usama Sarfraz <muhamedusama468@gmail.com>
2024-02-20 23:26:19 +0500

    cd end point added

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/cd', (req, res) => {
+  res.send('Continues Integrations and Deployment Added')
+})
+
 app.listen(3000, () => {
   console.log(`Example app listening on port 3000`)
 });
